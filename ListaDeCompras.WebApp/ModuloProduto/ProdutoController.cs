68ffc469dc880eb9291e7fef68b9d36806e3ac28
Michel <michelgabiatti07@gmail.com>
2026-06-04 23:04:48 -0300

    Criação de Produto

---
+++ ListaDeCompras.WebApp/ModuloProduto/ProdutoController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ListaDeCompras.WebApp.ModuloProduto
+{
+    public class ProdutoController : Controller
+    {
+        // GET: ProdutoController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
