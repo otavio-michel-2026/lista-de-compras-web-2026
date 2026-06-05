2c4e0f461f029a6eadb4bf5c17f4a4b3e70e9e3e
Michel <michelgabiatti07@gmail.com>
2026-06-04 23:00:55 -0300

    Criação do Controller

---
+++ ListaDeCompras.WebApp/ModuloCategoria/CategoriaController.cs
@@ -0,0 +1,114 @@
+using AutoMapper;
+using FluentResults;
+using ListaDeCompras.WebApp.Compartilhado.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ListaDeCompras.WebApp.ModuloCategoria;
+//Apresentação
+
+public class CategoriaController(ServicoCategoria servicoCategoria, IMapper mapeador) : Controller
+{
+    [HttpGet]
+    public ActionResult Listar()
+    {
+        var dtos = servicoCategoria.SelecionarTodos();
+
+        var listarVms = mapeador.Map<List<ListarCategoriaViewModel>>(dtos);
+
+        return View(listarVms);
+    }
+    [HttpGet]
+    public ActionResult Cadastrar()
+    {
+        CadastrarCategoriaViewModel cadastrarVm = new(
+            string.Empty,
+            string.Empty,
+            0
+        );
+
+        return View(cadastrarVm);
+    }
+    [HttpPost]
+    public ActionResult Cadastrar(CadastrarCategoriaViewModel vm)
+    {
+        if (!ModelState.IsValid)
+            return View(vm);
+
+        var dto = mapeador.Map<CadastrarCategoriaDto>(vm);
+
+        Result resultado = servicoCategoria.Cadastrar(dto);
+
+        if (resultado.IsFailed)
+        {
+            ModelState.AddModelError(resultado);
+
+            return View(vm);
+        }
+
+        return RedirectToAction(nameof(Listar));
+    }
+    [HttpGet]
+    public ActionResult Editar(Guid id)
+    {
+        var resultado = servicoCategoria.SelecionarPorId(id);
+
+        if (resultado.IsFailed)
+        {
+            TempData.AddErrorMessage(resultado);
+
+            return RedirectToAction(nameof(Listar));
+        }
+
+        var dto = resultado.Value;
+
+        var vm = mapeador.Map<EditarCategoriaViewModel>(dto);
+
+        return View(vm);
+    }
+    [HttpPost]
+    public ActionResult Editar(EditarCategoriaViewModel vm)
+    {
+        if (!ModelState.IsValid)
+            return View(vm);
+
+        var dto = mapeador.Map<EditarCategoriaDto>(vm);
+
+        Result resultado = servicoCategoria.Editar(dto);
+
+        if (resultado.IsFailed)
+        {
+            ModelState.AddModelError(resultado);
+            return View(vm);
+        }
+
+        return RedirectToAction(nameof(Listar));
+    }
+    [HttpGet]
+    public ActionResult Excluir(Guid id)
+    {
+        var resultado = servicoCategoria.SelecionarPorId(id);
+
+        if (resultado.IsFailed)
+        {
+            TempData.AddErrorMessage(resultado);
+
+            return RedirectToAction(nameof(Listar));
+        }
+
+        var dto = resultado.Value;
+
+        var vm = mapeador.Map<ExcluirCategoriaViewModel>(dto);
+
+        return View(vm);
+    }
+    [HttpPost]
+    public ActionResult Excluir(ExcluirCategoriaViewModel vm)
+    {
+        Result resultado = servicoCategoria.Excluir(vm.Id);
+
+        if (resultado.IsFailed)
+            TempData.AddErrorMessage(resultado);
+
+        return RedirectToAction(nameof(Listar));
+    }
+}
