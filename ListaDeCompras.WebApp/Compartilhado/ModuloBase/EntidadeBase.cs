68f87c594522dbbe461b3237936afce5a709bacd
Michel <michelgabiatti07@gmail.com>
2026-06-04 23:03:35 -0300

    Correção de erro relacionado com ID

--- ListaDeCompras.WebApp/Compartilhado/ModuloBase/EntidadeBase.cs
+++ ListaDeCompras.WebApp/Compartilhado/ModuloBase/EntidadeBase.cs
@@ -1,7 +1,9 @@
+using System;
+
 namespace ListaDeCompras.WebApp.Compartilhado.ModuloBase;
 // Domínio
 public abstract class EntidadeBase<T> where T : EntidadeBase<T>
 {
-    public Guid Id { get; set; }
+    public Guid Id { get; set; } = Guid.NewGuid();
     public abstract void Atualizar(T entidadeAtualizada);
 }
