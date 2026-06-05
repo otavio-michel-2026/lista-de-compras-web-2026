33445b108d009c8ab2d94f505e02b8bf63563687
Michel <michelgabiatti07@gmail.com>
2026-06-04 22:51:00 -0300

    Criacao da pagina inicial

---
+++ ListaDeCompras.WebApp/ModuloHome/HomeController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ListaDeCompras.WebApp.Compartilhado.ModuloBase
+{
+    public class HomeController : Controller
+    {
+        // GET: HomeController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
